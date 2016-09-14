02e5298526f31c60cb4c585747e6dbff1fa05f6f
skipness <skipness123@gmail.com>
2016-09-14 15:03:20 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,21 +3,21 @@
 var TelegramBot = require('node-telegram-bot-api');
 var Twit = require('twit');
 
-var token = '...';
+var token = '...'; //your telegram bot api token
 var bot = new TelegramBot(token, {polling: true});
 
 var Tweety = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...'
+  consumer_key:         '...', //your twitter consumer key
+  consumer_secret:      '...', //your twitter consumer secret
+  access_token:         '...', //your twitter access token
+  access_token_secret:  '...' //your twitter access token secret
 })
 
 var stream = Tweety.stream('statuses/filter', { follow: ['294025417'] });
 
 stream.on('tweet', function (tweet) {
     if (!tweet.retweeted_status && !tweet.quoted_status && !tweet.in_reply_to_user_id) {
-      bot.sendMessage('...', tweet.text); 
+      bot.sendMessage('...', tweet.text); // chat id
     }
 })
 
